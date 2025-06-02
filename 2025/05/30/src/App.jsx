25be1d5c5dd0877453d95f74f28cdea9d02ef13b
D4fto <163424876+D4fto@users.noreply.github.com>
2025-06-02 09:01:53 -0300

    up

--- 2025/05/30/src/App.jsx
+++ 2025/05/30/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import Header from "./HEader.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import MainContent from './MainContent.jsx';
 
 function App() {
-
   return (
     <div className='app'>
       <Header></Header>
+      <MainContent></MainContent>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ 2025/05/30/src/MainContent.jsx
@@ -0,0 +1,16 @@
+import Section from "./Section.jsx";
+import "./MainContent.css"
+
+export default function MainContent(){
+    return(
+        <main className="conteudo">
+            <Section title={"kdfhfoj hjs js dffsd j"}>
+                <p>kgsfdhfdji hfsd</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </Section>
+            <Section title={"bfhgfdplknhgsm"}>
+                <p>kgsfdhfdji hfsdhgd kdhgçl lkdhdgoiu hdgfudfgh gfdshfsdufsdh </p>
+            </Section>
+        </main>
+    )
+}
